822a54ee9e8ef6e66a3bbd510fe66de0733a5334
Kirill <kirill040605@gmail.com>
2023-03-09 10:34:29 -0800

    header developing is started

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,33 @@
-const gulp = require('gulp');
+const gulp        = require('gulp');
 const browserSync = require('browser-sync');
-const sass = require('gulp-sass')(require('sass'));
-const rename = require("gulp-rename");
-const autoprefixer = require('gulp-autoprefixer');
+const sass        = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
+const rename = require("gulp-rename");
 
+gulp.task('server', function() {
 
-gulp.task('server', () => {
     browserSync({
         server: {
             baseDir: "src"
         }
     });
+
+    gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
-gulp.task('styles', () => {
+gulp.task('styles', function() {
     return gulp.src("src/sass/**/*.+(scss|sass)")
-                .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-                .pipe(rename({
-                    prefix: '',
-                    suffix: '.min',
-                }))
-                .pipe(autoprefixer({
-                    browsers: ['last 2 versions'],
-                    cascade: false
-                }))
-                .pipe(cleanCSS({compatibility: 'ie8'}))
-                .pipe(gulp.dest("src/css"))
-                .pipe(browserSync.stream());
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(autoprefixer())
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('watch', () => {
-    gulp.watch('src/sass/**/*.+(scss|sass)', gulp.parallel('styles'));
-    gulp.watch('src/index.html').on('change', browserSync.reload);
-});
+gulp.task('watch', function() {
+    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
+})
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
