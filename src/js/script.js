e35a40ca4c5e7a69afd2b48d6bcf8c39963aa1d4
Kirill <kirill040605@gmail.com>
2023-03-10 17:19:32 -0800

    changed library for slider(from slick to tiny slider)

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,22 @@
-
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-        speed: 1000,
-        // adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
-        
-        
-    });
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    dots: false,
+    nav: false
+    
 });
+
+
+document.querySelectorAll('.slider-btn').forEach(item => {
+    item.addEventListener('click', (e) => {
+        const target = e.currentTarget.dataset['side'];
+        if(target === 'left') {
+            slider.goTo('prev');
+        }else {
+            slider.goTo('next');
+        }
+    })
+})
