cbf82c80a9da410140ac0d5416a3e74cbf70a756
Kirill <kirill040605@gmail.com>
2023-03-14 16:47:19 -0700

    new svg icons

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,26 @@
+
+$(document).ready(function() {
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active')
+            })
+        });
+    };
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+})
+
 const slider = tns({
     container: '.carousel__inner',
     items: 1,
@@ -5,11 +28,10 @@ const slider = tns({
     autoplay: false,
     controls: false,
     dots: false,
-    nav: false
+    nav: false,
+    
     
 });
-
-
 document.querySelectorAll('.slider-btn').forEach(item => {
     item.addEventListener('click', (e) => {
         const target = e.currentTarget.dataset['side'];
@@ -20,3 +42,5 @@ document.querySelectorAll('.slider-btn').forEach(item => {
         }
     })
 })
+
+
